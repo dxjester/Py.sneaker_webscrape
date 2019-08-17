f6cc273586058d112cd12bb435d7c90e45fbac7f
dxjester <patbenitez@gmail.com>
2019-08-17 10:32:21 -0400

    converted file structure to class / OOP format

--- sneakernews_scrape.py
+++ sneakernews_scrape.py
@@ -17,48 +17,80 @@ VERSION: 1.0
 # import the required modules
 import nltk 
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 from collections import Counter
 from string import punctuation
 
+# 1.1 Class Declaration ------------------------------------------------------#
+class sneaker_site:
+    def __init__ (self, name, url):
+        self.name = name
+        self.url = url
+        self.site_text = ''
+        self.lines = ''
+        
+        self.soup = ''
+        self.hyperlink_list = ''
+        self.paragraph_list = ''
+        
+        self.nike_site_count = 0
+        self.adidas_site_count = 0
+        self.reebok_site_count = 0
+        self.new_balance_site_count = 0
+        self.puma_site_count = 0
 
-# retrieve the url 
-url = 'https://sneakernews.com/'
-r = requests.get(url)
-type(r)
+        self.nike_master = ['Nike', 'Air', 'Max', 'Jordan', 'Zoom', 'React', 'Shox', 'ACG', 'Max Plus', 'Joyride', 'Tinker', 'Force', 'Westbrook', 'Kyrie','Lebron', 'Durant', 'SB', 'Air Max 90', 'Air Max 1', 'Kyrie', 'Air Max 270', 'Travis Scott' ]
+        self.adidas_master = ['Adidas', 'ADIDAS', 'adidas', 'Yeezy', 'Kanye', 'Ultraboost', 'Ultra Boost', 'FYW', 'Harden']
+        self.new_balance_master = ['New Balance', 'NB', 'Balance', '997', '801']
+        self.puma_master = ['Puma', 'Cell Venom', 'Thunder Spectre', 'Clyde Court']
 
-# utilize Beautiful soup to read in 
-soup = BeautifulSoup(r.content, "html.parser")
-type(soup)
+        self.sneaker_list = self.nike_master + self.adidas_master + self.new_balance_master + self.puma_master
+        self.length = len(self.sneaker_list)         
+        
+    def site_calculate(self):
+        r = requests.get(self.url)
+        self.soup = BeautifulSoup(r.content, "html.parser")
+        
+        print("\nConsolidating all hyperlinks and paragraphs in the webpage \n" )        
+        self.hyperlink_list = self.soup.findAll('a')
+        self.paragraph_list = self.soup.findAll('p')
+        
+        self.site_text = self.soup.get_text()
+        print("\nConverting ", self.name, " to text file ... \n")
+        
+        self.lines = [self.site_text.lower() for line in self.site_text]
+        print("\nCalculating individual counts: " )
+        for item in self.sneaker_list:
+            name = item + ': '
+            count = self.site_text.count(item)
+    
+            if count > 0:
+                if item in self.nike_master:
+                    self.nike_site_count += count
+                elif item in self.adidas_master:
+                    self.adidas_site_count += count
+                elif item in self.new_balance_master:
+                    self.new_balance_site_count += count
+                elif item in self.puma_master:
+                    self.puma_site_count += count     
+                else: 
+                    0 
+            print(name, count)
+            
+    def display_info(self):
+        print("\nCalculating total counts by shoe company...")
+        print("Total Nike mentions: ", self.nike_site_count)
+        print("Total Adidas mentions: ", self.adidas_site_count)
+        print("Total New Balance mentions: ", self.new_balance_site_count)
+        print("Total Puma mentions: ", self.puma_site_count)       
+        print(self.lines)          
+  
+#----------------------------------- START -----------------------------------#
+#-------------------------- PHASE 2: Execution area --------------------------#
+#-----------------------------------------------------------------------------#
 
-soup.title
+sneaker_news = sneaker_site('sneakernews.com', 'https://sneakernews.com/')
+sneaker_news.site_calculate()
+sneaker_news.display_info()
 
-# retrieve all hyperlinks within the webpage and store as a list
-hyperlink_list = soup.findAll('a')
-hyperlink_list
-
-# find all paragraphs in the webpage
-hyperlink_list = soup.findAll('p')
-hyperlink_list
-
-# convert the ingest as a text
-sneaker_text = soup.get_text()
-print(sneaker_text)
-
-lines = [sneaker_text.lower() for line in sneaker_text]
-lines
-
-
-nike_list = ['Nike', 'Air', 'Max', 'Jordan', 'Zoom', 'React', 'Shox', 'ACG', 'Max Plus', 'Joyride', 'Tinker', 'Force', 'Westbrook', 'Kyrie','Lebron', 'Durant', 'SB', 'Air Max 90', 'Air Max 1', 'Kyrie', 'Air Max 270', 'Travis Scott' ]
-adidas_list = ['Adidas', 'Yeezy', 'Kanye', 'Ultraboost', 'Ultra Boost', 'FYW', 'Harden']
-new_balance_list = ['New Balance', 'NB', 'Balance', '997']
-sneaker_list = nike_list + adidas_list + new_balance_list
-length = len(sneaker_list) 
-
-nike_count = sneaker_text.count("Nike")
-nike_count
-
-for item in sneaker_list:
-    name = item + ': '
-    count = sneaker_text.count(item)
-    print(name, count)
