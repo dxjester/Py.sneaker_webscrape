000c8aef3fa96651daa01573907fa646c84b55d6
dxjester <patbenitez@gmail.com>
2019-08-18 16:19:16 -0400

    commented out individual shoe models, replaced with company names

--- sneakernews_scrape.py
+++ sneakernews_scrape.py
@@ -18,6 +18,8 @@ VERSION: 1.0
 import nltk 
 import requests
 import pandas as pd
+import datetime
+from datetime import date 
 from bs4 import BeautifulSoup
 from collections import Counter
 from string import punctuation
@@ -29,6 +31,7 @@ class sneaker_site:
         self.url = url
         self.site_text = ''
         self.lines = ''
+        self.site_df = pd.DataFrame(columns = ['date', 'dtg','category_name', 'item', 'count'])
         
         self.soup = ''
         self.hyperlink_list = ''
@@ -40,10 +43,14 @@ class sneaker_site:
         self.new_balance_site_count = 0
         self.puma_site_count = 0
 
-        self.nike_master = ['Nike', 'Air', 'Max', 'Jordan', 'Zoom', 'React', 'Shox', 'ACG', 'Max Plus', 'Joyride', 'Tinker', 'Force', 'Westbrook', 'Kyrie','Lebron', 'Durant', 'SB', 'Air Max 90', 'Air Max 1', 'Kyrie', 'Air Max 270', 'Travis Scott' ]
-        self.adidas_master = ['Adidas', 'ADIDAS', 'adidas', 'Yeezy', 'Kanye', 'Ultraboost', 'Ultra Boost', 'FYW', 'Harden']
-        self.new_balance_master = ['New Balance', 'NB', 'Balance', '997', '801']
-        self.puma_master = ['Puma', 'Cell Venom', 'Thunder Spectre', 'Clyde Court']
+        self.nike_master = ['Nike', 'Jordan', 'Converse']
+        # ['Nike', 'Air', 'Max', 'Jordan', 'Zoom', 'React', 'Shox', 'ACG', 'Max Plus', 'Joyride', 'Tinker', 'Force', 'Westbrook', 'Kyrie','Lebron', 'Durant', 'SB', 'Air Max 90', 'Air Max 97', 'Air Max 1', 'Kyrie', 'Air Max 270', 'Travis Scott' ]
+        self.adidas_master = ['Adidas', 'Reebok', 'ADIDAS', 'Yeezy', 'Kanye']
+        # ['Adidas', 'ADIDAS', 'adidas', 'Yeezy', 'Kanye', 'Ultraboost', 'EQT', 'NMD', 'Ultra Boost', 'FYW', 'Harden']
+        self.new_balance_master = ['New Balance', 'NB']
+        # ['New Balance', 'NB', 'Balance', '997', '801']
+        self.puma_master = ['Puma', 'Cell Venom']
+        #['Puma', 'Cell Venom', 'Thunder Spectre', 'Clyde Court']
 
         self.sneaker_list = self.nike_master + self.adidas_master + self.new_balance_master + self.puma_master
         self.length = len(self.sneaker_list)         
@@ -61,29 +68,51 @@ class sneaker_site:
         
         self.lines = [self.site_text.lower() for line in self.site_text]
         print("\nCalculating individual counts: " )
+        
+        index_num = 0
         for item in self.sneaker_list:
             name = item + ': '
             count = self.site_text.count(item)
-    
+            day = date.today()
+            dtg = datetime.datetime.now()
+            category = ''
             if count > 0:
                 if item in self.nike_master:
                     self.nike_site_count += count
+                    category = 'Nike'
                 elif item in self.adidas_master:
                     self.adidas_site_count += count
+                    category = 'Adidas'
                 elif item in self.new_balance_master:
                     self.new_balance_site_count += count
+                    category = 'New Balance'
                 elif item in self.puma_master:
-                    self.puma_site_count += count     
+                    self.puma_site_count += count
+                    category = 'Puma'
                 else: 
-                    0 
+                    0
+            else: 
+                if item in self.nike_master:
+                    category = 'Nike'
+                elif item in self.adidas_master:
+                    category = 'Adidas'
+                elif item in self.new_balance_master:
+                    category = 'New Balance'
+                elif item in self.puma_master:
+                    category = 'Puma'
+                else: 
+                    0                
+            self.site_df.loc[index_num] = [day, dtg, category, item, count]        
             print(name, count)
+            index_num += 1
             
     def display_info(self):
         print("\nCalculating total counts by shoe company...")
         print("Total Nike mentions: ", self.nike_site_count)
         print("Total Adidas mentions: ", self.adidas_site_count)
         print("Total New Balance mentions: ", self.new_balance_site_count)
-        print("Total Puma mentions: ", self.puma_site_count)                
+        print("Total Puma mentions: ", self.puma_site_count)      
+        print(self.site_df)          
   
 #----------------------------------- START -----------------------------------#
 #-------------------------- PHASE 2: Execution area --------------------------#
