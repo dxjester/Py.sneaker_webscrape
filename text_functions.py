f567877abd3ddbab9a13f3f44d5fc26be43a90ed
dxjester <patbenitez@gmail.com>
2019-11-10 18:21:39 -0500

    added new text funciton file to project

---
+++ text_functions.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+FILENAME: text_functions.py
+PROJECT: Sneaker Website Analysis
+DATE CREATED: 10-Nov-19
+DATE UPDATED: 9-Nov-19
+VERSION: 1.0
+"""
+
+
+#----------------------------------- START -----------------------------------#
+#-------------------------- PHASE 1: Import Libraries ------------------------#
+#-----------------------------------------------------------------------------#
+
+
+# 1.1 Normalize Text ------ --------------------------------------------------#
+
+def normalize_string(s):
+    '''
+    
+    '''
+    assert type (s) is str
+    
+
