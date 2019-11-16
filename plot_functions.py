f48ad7ab40cc5a900191ffd8fa8da523000ba6dc
dxjester <patbenitez@gmail.com>
2019-11-16 11:23:50 -0500

    added pie chart function

--- plot_functions.py
+++ plot_functions.py
@@ -17,9 +17,9 @@ import matplotlib.pyplot as plt
 
 def bar_chart(df, x_axis, y_axis, title):
     '''
-    NAME: bar_chart
+    NAME: bar_chart function
     PURPOSE: given a dataframe, two columns, and a title plot a bar chart
-    COMMENTS: commented out color code lines
+    COMMENTS: only allows one (1) x categorical variable and one (1) x quantifiable variable
     '''
     
     color_list = ['palegreen','lightsteelblue','wheat','turquoise','silver', 'salmon','palegreen', 'peachpuff']
@@ -44,4 +44,28 @@ def bar_chart(df, x_axis, y_axis, title):
     #ax.bar(x_val, y_val, align='center')
     chart_title = title + ' Category Chart'
     ax.title.set_text(chart_title)
+    plt.show()
+    
+def pie_chart(df, x_axis, y_axis, title):
+    '''
+    NAME: pie_chart function
+    PURPOSE: given a dataframe, two columns, and a title, plot a pie chart
+    COMMENTS: only allows one (1) x categorical variable and one (1) x quantifiable variable
+    '''
+    color_list = ['palegreen','lightsteelblue','wheat','turquoise','silver', 'salmon','palegreen', 'peachpuff']
+    # create a new dataframe with the two provided categories
+    sliced_df = df[[x_axis,y_axis]]
+    summed_df = sliced_df.groupby([x_axis]).sum()
+    
+    sorted_df = summed_df.sort_values('count', ascending = False)
+    master_df = sorted_df.reset_index()
+    
+    x_val = master_df[x_axis]
+    y_val = master_df[y_axis]
+    
+    x_length = len(x_val)
+    cr = color_list[:x_length]
+    plt.pie(y_val, labels=x_val, colors=cr, autopct='%1.1f%%', shadow=True, startangle=140)
+    
+    plt.axis('equal')
     plt.show()
